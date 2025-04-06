42eeab01ff88e365241bc684e2a9f469b6be0f2f
TilakMaddy <tilak.madichetti@gmail.com>
2025-04-06 11:44:39 +0530

    wip

--- crates/ast/src/cnodes/consts.rs
+++ crates/ast/src/cnodes/consts.rs
@@ -13,38 +13,3 @@ pub enum CConstSign {
     Plus,
     Minus,
 }
-
-// Semantic analysis
-// Should be done in multiple passes/steps
-// Errors should return a format that can be displayed
-// That means each node in CIFace must contain a SourceLocation
-// - source-unit (id)
-// - byte offset
-// - char offset
-// - help message
-// (ariadne crate requirements)
-// rename to ciface to cnode
-// create a new crate sema - for semantic analysis of cnode
-//impl Validate for CConst {
-//    type Result = super::Result<()>;
-//
-//    fn validate_ciface(&self) -> Self::Result {
-//        type Error = super::Error;
-//        match self {
-//            CConst::SignedInt { value, .. } => {
-//                if !value.chars().all(|c| c.is_ascii_digit()) {
-//                    let error_message = format!("non ascii digits detected in {}", value);
-//                    return Err(Error::CConst(error_message));
-//                }
-//                return Ok(());
-//            }
-//            CConst::HexDigit(value) => {
-//                if !value.chars().all(|c| c.is_ascii_hexdigit()) {
-//                    let error_message = format!("non ascii hex-digits detected in {}", value);
-//                    return Err(Error::CConst(error_message));
-//                }
-//                return Ok(());
-//            }
-//        }
-//    }
-//}
